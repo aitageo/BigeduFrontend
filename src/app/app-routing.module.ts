69e629b4aff2dd98fbb38278a43e07dcda0b0fd6
aitageo <aitageo1980@hotmail.com>
2022-11-25 00:20:25 -0500

    barra de navegacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ContactenosComponent } from './contactenos/contactenos.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
 
 const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'home',component:HomeComponent},
+  {path:'nosotros',component:NosotrosComponent},
+  {path:'contactenos',component:ContactenosComponent},
   {path:'login',component:LoginComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { ContactenosComponent } from './contactenos/contactenos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    NosotrosComponent,
+    ContactenosComponent
   ],
   imports: [
     BrowserModule,
